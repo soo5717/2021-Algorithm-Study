bea6887d3bbed35fe7d01552dca34b680b92adea
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-03-19 14:31:48 +0900

    [Programmers-42860] 조이스틱

--- Programmers/eugene/greedy/조이스틱.py
+++ Programmers/eugene/greedy/조이스틱.py
@@ -1,38 +1,27 @@
 def solution(name):
     answer = 0
-    
     name=list(name)
     i=0
     
     while(True):
-        left=1
-        right=1
-
+        left, right =1,1
+        
         #알파벳 카운트 : A가 아니면 알파벳 변경을 시도
         if name[i] != 'A': 
-            if( ord(name[i]) - ord('A') < 13) :
-                answer+=ord(name[i]) - ord('A')
-            else :
-                answer+=ord('Z')-ord(name[i])+1
-        #변경한 위치는 A로 바꾼다.
+            min(ord(name[i]) - ord('A'), ord('Z')-ord(name[i])+1)
+       
+        #변경한 위치는 A로 변경하고, 모든 문자열이 A로 바뀌면 종료.
         name[i] = 'A'
-                  
-        #종료 조건 : 모든 위치가 A로 바뀌면 종료한다. 
         if ['A'] * len(name) == name:
             break
             
         #커서 이동 카운트
         for k in range(1,len(name)):
-            if name[i+k]=="A": 
-                right+=1
-            else: 
-                break
-                
+            if name[i+k]=="A": right+=1
+            else : break               
         for k in range(1,len(name)):
-            if name[i-k]=="A": 
-                left+=1               
-            else: 
-                break
+            if name[i-k]=="A" : left+=1               
+            else: break
         
         #left/right 중 더 가까운 방향으로 이동        
         if left<right:
