461f2a90e8779b434b83bfbff3ed624913e73b0d
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-03-24 19:04:50 +0900

    [Programmers-42883] 큰 수 만들기

--- Programmers/eugene/greedy/큰 수 만들기.py
+++ Programmers/eugene/greedy/큰 수 만들기.py
@@ -2,12 +2,9 @@ def solution(number, k):
     stack = [number[0]]
     for num in number[1:]:
 
-        while len(stack) > 0 and stack[-1] < num and k > 0:
+        while stack and stack[-1]<num and k>0:
             k -= 1
             stack.pop()
-
         stack.append(num)
 
-    if k != 0:
-        stack = stack[:-k]
-    return ''.join(stack)
+    return ''.join(stack[:len(stack) - k])
