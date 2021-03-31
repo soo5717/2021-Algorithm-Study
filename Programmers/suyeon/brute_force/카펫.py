ff131dddbd4e0ad7b1fb6c40eb73d45e01843d07
soo5717 <koo5717@naver.com>
2021-03-31 18:45:50 +0900

    [Programmers-42839] 소수 찾기

--- Programmers/suyeon/brute_force/카펫.py
+++ Programmers/suyeon/brute_force/카펫.py
@@ -1,5 +1,7 @@
 def solution(brown, yellow):
     total = brown + yellow
+    # x(세로) <= total//x(가로) 이고, 
+    # x는 최소 3 이상이기 때문에 total//3 범위까지 반복함
     for x in range (3, total // 3 + 1):
         if total % x == 0 and brown ==  2 * (x + total // x - 2):
             return [total // x, x]
