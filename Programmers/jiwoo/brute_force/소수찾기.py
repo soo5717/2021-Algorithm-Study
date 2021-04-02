f24d4bffd49fd78023beabb89f1435cdf75a71d9
jjuyaa <smwujjuyaa@gmail.com>
2021-04-02 18:51:58 +0900

    [Programmers-42839, 42842] 소수찾기, 카펫

--- Programmers/jiwoo/brute_force/소수찾기.py
+++ Programmers/jiwoo/brute_force/소수찾기.py
@@ -15,7 +15,7 @@ def isPrime(n):
     if n == 0 or n == 1:
         return False
     else:
-        for i in range(2, n):
+        for i in range(2, n**0.5+1):
             if n % i == 0:
                 return False
         return True
--- Programmers/jiwoo/brute_force/카펫.py
+++ Programmers/jiwoo/brute_force/카펫.py
@@ -1,11 +1,8 @@
-import math
-
 def solution(brown, yellow):
     answer = []
-    for i in range(3, int(math.sqrt(brown+yellow))+1):
-        if (brown+yellow) % i == 0:
-            a = int((brown+yellow) // i)
-            b = i
-            if (a-2)*(b-2) == yellow:
-                break
-    return [a, b]
+    sum = brown+yellow
+
+    for i in range(3, (sum)**0.5+1):
+        if (sum) % i == 0:
+            if (sum//i-2)*(i-2) == yellow:
+                return [sum//i, i]
