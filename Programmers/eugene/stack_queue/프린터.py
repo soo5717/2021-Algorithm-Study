7f71ed136a415e8ce075de36180866acc90bd25a
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-04-07 18:41:21 +0900

    [Programmers-42587] 프린터

--- Programmers/eugene/stack_queue/프린터.py
+++ Programmers/eugene/stack_queue/프린터.py
@@ -1,21 +1,23 @@
+from collections import deque
+
 def solution(priorities, location):
     answer = 0
     
-    while(priorities):
+    queue=deque(priorities)
+    while(queue):
         if location == 0 : #location이 맨 앞으로 왔을 경우
-            if priorities[0] < max(priorities): #더 큰 중요도가 뒤에 있으면
-                priorities.append(priorities.pop(0))
-                location = len(priorities)-1
+            if queue[0] < max(queue): #더 큰 중요도가 뒤에 있으면
+                queue.append(queue.popleft())
+                location = len(queue)-1
             else : #loca에 있는 문서의 중요도가 가장 높으면 출력하고 순서 리턴
                 return answer+1
-            
-            
+
         else : #아직 loca차례가 오지 않았을 때
             location -=1
-            if priorities[0] < max(priorities):
-                priorities.append(priorities.pop(0))
+            if queue[0] < max(queue):
+                queue.append(queue.popleft())
                 
             else :
-                priorities.pop(0) #
+                queue.popleft()
                 answer+=1    
     return answer
