bbaf9aaa14786df81e4d20535be0a7817b583ff9
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-04-09 09:45:33 +0900

    [Programmers-43165] 타겟 넘버

--- Programmers/eugene/dfs_bfs/타겟 넘버.py
+++ Programmers/eugene/dfs_bfs/타겟 넘버.py
@@ -2,7 +2,7 @@ from collections import deque
 
 def solution(numbers, target):
     
-    queue = deque([0]) #numbers 순서에 따라 매번 변한다.
+    queue = deque([0])
     for n in numbers :
         sub_q = []
         for i in queue :
