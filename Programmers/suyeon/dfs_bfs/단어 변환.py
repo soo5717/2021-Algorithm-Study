b2668189f7cccec69042e1953ec979a2ee2c2872
soo5717 <koo5717@naver.com>
2021-04-29 00:57:07 +0900

    [Programmers-43163] 단어 변환

--- Programmers/suyeon/dfs_bfs/단어 변환.py
+++ Programmers/suyeon/dfs_bfs/단어 변환.py
@@ -1,30 +1,16 @@
-from collections import deque
-
-def dfs(begin, target, words, visited):
-    answer = 0
-    
-    queue = deque([begin])
-    while queue:
-        begin = queue.pop()
-        if begin == target: 
-            break
-        
-        for i, word in enumerate(words):
-            if len([j for j in range(len(word)) if begin[j] != word[j]]) == 1:
-                if not visited[i]:
-                    # print(word)
-                    queue.append(word)
-                    visited[i] = True
-        print(queue)
-        answer += 1
-    
-    if begin != target: 
-        return 0
-    
-    return answer
-    
 def solution(begin, target, words):
-    # 단어가 없는 경우
-    if target not in words: return 0
-    
-    return dfs(begin, target, words, [False] * len(words))
+    answer = 0
+    stack = [begin]
+
+    while True:
+        temp = []
+        for word in stack:
+            if word == target: return answer
+            
+            for i in range(len(words)-1, -1, -1): # Index Error 방지
+                if sum([x!=y for x, y in zip(word, words[i])]) == 1:
+                    temp.append(words.pop(i))
+        # print(temp)
+        if not temp: return 0
+        stack = temp
+        answer += 1
