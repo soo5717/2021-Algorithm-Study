e06ce65fe4d732556c368a99444e290afa7765b8
soo5717 <koo5717@naver.com>
2021-05-05 03:16:47 +0900

    [Programmers-42578] 위장

--- Programmers/suyeon/hash/위장.py
+++ Programmers/suyeon/hash/위장.py
@@ -1,9 +1,12 @@
+# Counter, reduce를 사용한 풀이도 가능
+# get 대신 defauldict 사용 가능
+
 def solution(clothes):
     dictionary = dict()
-    for cloth in clothes:
-        dictionary[cloth[-1]] = dictionary.get(cloth[-1], 0) + 1 
+    for name, kind in clothes:
+        dictionary[kind] = dictionary.get(kind, 1) + 1 
     
     answer = 1
     for value in dictionary.values():
-        answer *= value + 1
+        answer *= value
     return answer - 1
