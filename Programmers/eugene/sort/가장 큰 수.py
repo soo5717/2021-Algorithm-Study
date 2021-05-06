deeef98582da0c3ff8fddbaaa08d28f90002c66d
eugene-tp <ttpp05152@sookmyung.ac.kr>
2021-05-06 12:54:07 +0900

    [Programmers-42746] 가장 큰 수

--- Programmers/eugene/sort/가장 큰 수.py
+++ Programmers/eugene/sort/가장 큰 수.py
@@ -2,6 +2,5 @@ def solution(numbers):
     answer = ''
     
     numbers=list(map(str, numbers))
-    sort=sorted(numbers, key=lambda x:x*3, reverse=True)
-
-    return str(int(''.join(sort)))
+    numbers.sort(key=lambda x:x*3, reverse=True)
+    return str(int(''.join(numbers)))
