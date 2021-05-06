fee4bc701018e5782507f53330d6e3bbd4bdb66c
eugene-tp <ttpp05152@sookmyung.ac.kr>
2021-05-06 12:53:18 +0900

    [Programmers-42748] K번째수

--- Programmers/eugene/sort/K번째수.py
+++ Programmers/eugene/sort/K번째수.py
@@ -1,8 +1,2 @@
 def solution(array, commands):
-    answer = []
-    
-    for command in commands:
-        temp = array[command[0]-1 : command[1]]
-        temp.sort()
-        answer.append(temp[command[2]-1])
-    return answer
+    return [sorted(array[i-1 :j])[k-1] for i,j,k in commands]
