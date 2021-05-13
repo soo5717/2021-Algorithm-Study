a1f26c9df9c8676b0d0fcc2ed52b72bbfb8b4054
jjuyaa <smwujjuyaa@gmail.com>
2021-05-13 14:03:51 +0900

    [Programmers-43163] 단어변환

--- Programmers/jiwoo/dfs_bfs/단어변환.py
+++ Programmers/jiwoo/dfs_bfs/단어변환.py
@@ -1,26 +1,17 @@
 def solution(begin, target, words):
-    answer = [begin]
-
-    if target not in words:
-        return 0
-    
-    levelCount = 0
-    while len(words):
-        for beginWord in answer:
-            change = []
-            for word in words:
-                count = 0
-                for i in range(len(beginWord)):
-                    if beginWord[i] != word[i]:
-                        count += 1
-                    if count == 2:
-                        break
-                if count == 1:
-                    change.append(word)
-                    words.remove(word)
-        levelCount += 1
-        if target in change:
-            return levelCount
-        else:
-            answer = change
-    return 0
+    answer = 0
+    queue = [begin]
+    while True:
+        temp = []
+        for word in queue:
+            if word == target:
+                return answer
+            for index in range(len(words)-1, -1, -1):
+                word2 = words[index]
+                difference = sum([x != y for x, y in zip(word, word2)])
+                if difference == 1:
+                    temp.append(words.pop(index))
+        if not temp:
+            return 0
+        queue = temp
+        answer += 1
