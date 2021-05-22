f7388d7a796ad1a7d6e9286dda6e2bdaccea8172
soo5717 <koo5717@naver.com>
2021-05-22 23:47:23 +0900

    [Programmers-42895] N으로 표현

--- Programmers/suyeon/dynamic_programming/N으로 표현.py
+++ Programmers/suyeon/dynamic_programming/N으로 표현.py
@@ -7,9 +7,10 @@ def solution(N, number):
         
         for j in range(1, i // 2 + 1):
             for num_1 in DP[j]:
-                for num_2 in DP[i - j]:
+                for num_2 in DP[i - j]: 
                     temp.add(num_1 + num_2)
                     temp.add(num_1 - num_2)
+                    temp.add(num_2 - num_1)
                     temp.add(num_1 * num_2)
                     if num_2: temp.add(num_1 // num_2)
                     if num_1: temp.add(num_2 // num_1)
