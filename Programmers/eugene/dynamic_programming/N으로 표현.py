b247c0cb5a4c477880f727c671aeb46de8c8201a
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-05-26 18:22:22 +0900

    [Programmers-42895] N으로 표현

--- Programmers/eugene/dynamic_programming/N으로 표현.py
+++ Programmers/eugene/dynamic_programming/N으로 표현.py
@@ -1,25 +1,20 @@
 def solution(N, number):
-    if(N==number):
-        return 1
-    temp=[[N]]
-    for i in range(1,8):
-        temp.append([])
-        cnt=i+1
-        #[cnt-k,k]를 돌면서 모든 경우를 구한다.
-        temp[i].append(int(str(N)*cnt))
+    if N == number: return 1
 
-        #[cnt-1,1] [cnt-2,2] [cnt-k, y=k]
-        for k in range(1,cnt//2+1): 
-            for x in temp[cnt-k-1]:
-                for y in temp[k-1]:
-                    temp[i].append(x+y)
-                    temp[i].append(x-y) 
-                    temp[i].append(y-x) 
-                    temp[i].append(x*y)    
-                    temp[i].append(x//y)
-        temp[i]=list(set(temp[i])) #중복, 0제거
-        if 0 in temp[i]:
-            temp[i].remove(0)   
-        if number in temp[i]:
-            return cnt
+    DP = [{}, { N }]
+    for i in range(2, 9):
+        temp = { int(str(N) * i) }
+
+        for j in range(1, i//2 + 1):
+            for x in DP[j]:
+                for y in DP[i - j]:
+                    temp.add(x + y)
+                    temp.add(x - y)
+                    temp.add(x - y)
+                    temp.add(x * y)
+                    if y: temp.add(x // y)
+                    if x: temp.add(y // x)
+
+                    if number in temp: return i
+        DP.append(temp)
     return -1
