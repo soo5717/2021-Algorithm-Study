d1b16ddb08decfceceb76c830f09d97d477d07b2
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-05-26 18:16:01 +0900

    [Programmers-43105] 정수 삼각형

--- Programmers/eugene/dynamic_programming/정수 삼각형.py
+++ Programmers/eugene/dynamic_programming/정수 삼각형.py
@@ -1,17 +1,11 @@
 def solution(triangle):
-    answers=[triangle[0]]
-    for i, tr in enumerate(triangle): #전체
-        if i==0: continue
-        temp=[]
-        for j, value in enumerate(tr): #한 줄씩
-            #맨 처음과 끝은 바로 위 answers[i-1][0], answers[i-1][i]를 value에 더해준다.
-            #나머지 위치들은 양옆을 비교해서 큰 값을 더해준다.
+    for i in range (1,len(triangle)): #전체
+        for j in range(i+1): #한 줄씩
             if (j==0): #줄의 맨 처음
-                temp.append(answers[i-1][0]+value)
+                triangle[i][j] += triangle[i-1][0]
             elif j==i : #줄의 맨 끝
-                temp.append(answers[i-1][i-1]+value)
+                triangle[i][j] += triangle[i-1][-1]
             else : #중간
-                temp.append(max(answers[i-1][j-1], answers[i-1][j])+value)
-        answers.append(temp)
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
     #전체 다 돌고, 마지막 temp에서 가장 큰 값 리턴
-    return max(temp)
+    return max(triangle[-1])
