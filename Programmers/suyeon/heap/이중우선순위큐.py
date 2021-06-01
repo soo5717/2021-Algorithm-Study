6e321004fcca08213c3a6576d9fe052c90f2a70f
soo5717 <koo5717@naver.com>
2021-06-02 02:16:13 +0900

    [Programmers-42628] 이중우선순위큐

--- Programmers/suyeon/heap/이중우선순위큐.py
+++ Programmers/suyeon/heap/이중우선순위큐.py
@@ -1,17 +1,20 @@
 from heapq import heapify, heappop, heappush
 
 def solution(operations):
-    heap = []
-    for op in operations:
-        if op == "D 1": #최댓값 삭제 
-            if not heap: continue
-            heap.remove(max(heap))
-            heapify(heap)
-        elif op == "D -1": #최솟값 삭제
-            if not heap: continue
-            heappop(heap)
-        else:
-            _, num = op.split()
-            heappush(heap, int(num))
+    min_heap, max_heap = [], []
     
-    return [max(heap), heap[0]] if heap else [0, 0]
+    for op in operations:    
+        if op == "D 1": #최댓값 삭제
+            if not max_heap: continue
+            max_num = heappop(max_heap)[1]
+            min_heap.remove(max_num)
+        elif op == "D -1": #최솟값 삭제
+            if not min_heap: continue
+            min_num = heappop(min_heap)
+            max_heap.remove((-min_num, min_num))
+        else:
+            num = int(op[2:])
+            heappush(min_heap, num)
+            heappush(max_heap, (-num, num))
+    
+    return [max_heap[0][1], min_heap[0]] if max_heap and min_heap else [0, 0]
