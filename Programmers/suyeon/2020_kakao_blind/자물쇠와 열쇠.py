ba33500f42c9b200dfaaa097fa2a13b68a6531d7
suyeon <soo5717@sookmyung.ac.kr>
2021-07-07 18:44:00 +0900

    [Programmers-60059] 자물쇠와 열쇠

---
+++ Programmers/suyeon/2020_kakao_blind/자물쇠와 열쇠.py
@@ -0,0 +1,28 @@
+def get_rotation(keys, m): # 90 rotation
+    matrix = [[0] * m for _ in range(m)]
+    for i in range(m):
+        for j in range(m):
+            matrix[i][j] = keys[m - 1 - j][i]
+    return matrix
+
+def solution(keys, locks):
+    m, n = len(keys), len(locks)
+    for angle in range(0, 360, 90):
+        if angle != 0: 
+            keys = get_rotation(keys, m)
+            
+        for x in range(m + n):
+            for y in range(m + n):
+                matrix = [[0] * (n + 2 * m) for _ in range(n + 2 * m)]
+                for i in range(m):
+                    for j in range(m):
+                        matrix[x + i][y + j] = keys[i][j]
+                
+                answer = True
+                for i in range(n):
+                    for j in range(n):
+                        if matrix[m + i][m + j] ^ locks[i][j] == 0:
+                            answer = False
+                            break
+                if answer: return True
+    return False
