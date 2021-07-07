a33676ff374812395024e74d3d1c0b24acc43d07
jjuyaa <smwujjuyaa@gmail.com>
2021-07-07 17:06:29 +0900

    [Programmers-72412] 순위 검색

--- Programmers/jiwoo/2021_kakao_blind/순위검색.py
+++ Programmers/jiwoo/2021_kakao_blind/순위검색.py
@@ -1,24 +1,47 @@
-def solution(info, query):
+from collections import defaultdict
+from itertools import combinations
+
+def solution(infos, queries):
     answer = []
-    for q in query:
-        count = 0
-        order = q.split()
-        for i in info:
-            person = i.split()
-            if order[0] != "-":
-                if order[0] != person[0]:
-                    continue
-            if order[2] != "-":
-                if order[2] != person[1]:
-                    continue
-            if order[4] != "-":
-                if order[4] != person[2]:
-                    continue
-            if order[6] != "-":
-                if order[6] != person[3]:
-                    continue
-            if int(person[4]) >= int(order[7]):
-                count += 1
-        answer.append(count)
-                
+    info_dic = defaultdict(list)
+    for info in infos:
+        info = info.split()
+        info_ = info[:-1]
+        info_score = int(info[-1])
+        
+        # 가능한 info 조합 생성하여, key, score로 저장
+        for i in range(5):
+            for c in combinations(info_, i):
+                temp_key = ''.join(c)
+                info_dic[temp_key].append(info_score)
+    
+    # score 오름차순 정렬
+    for key in info_dic.keys():
+        info_dic[key].sort()
+       
+    for query in queries:
+        query = query.split()
+        query_score = int(query[-1])
+        query_ = query[:-1]
+        
+        for i in range(3):
+            query_.remove('and')
+        while '-' in query_:
+            query_.remove('-')
+        query_ = ''.join(query_)
+        
+        if query_ in info_dic:
+            scores = info_dic[query_]
+            if len(scores) > 0:
+                start, end = 0, len(scores)
+                while end > start:
+                    mid = (start+end) // 2
+                    if scores[mid] >= query_score:
+                        end = mid
+                    else:
+                        start = mid+1
+                answer.append(len(scores)-start)
+        else:
+            answer.append(0)
+    
     return answer
