58d609eba24bcb28f0b5677a3be0dfd20e21ecd3
jjuyaa <smwujjuyaa@gmail.com>
2021-07-20 19:49:21 +0900

    [Programmers-42889] 실패율

--- Programmers/jiwoo/2019_kakao_blind/실패율.py
+++ Programmers/jiwoo/2019_kakao_blind/실패율.py
@@ -1,12 +1,13 @@
+from collections import defaultdict
+
 def solution(N, stages):
+    stage_dict = defaultdict(int)
+    for stage in stages:
+        stage_dict[stage] += 1
+        
     result = {}
     users = len(stages)
-    
     for i in range(1, N+1):
-        if users != 0:
-            count = stages.count(i)
-            result[i] = (count/users)
-            users -= count
-        else:
-            result[i] = 0
+        users -= stage_dict[i-1]
+        result[i] = stage_dict[i]/users if users else 0
     return sorted(result, key=lambda x: result[x], reverse = True)
