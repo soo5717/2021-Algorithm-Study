bb32b7c1485c03748e1698a87d7c93650dec45ed
jjuyaa <smwujjuyaa@gmail.com>
2021-07-20 19:52:30 +0900

    [Programmers-42888] 오픈채팅방

--- Programmers/jiwoo/2019_kakao_blind/오픈채팅방.py
+++ Programmers/jiwoo/2019_kakao_blind/오픈채팅방.py
@@ -3,18 +3,16 @@ def solution(records):
     user_nick = {}
     
     for record in records:
-        record_split = record.split(' ')
+        record_split = record.split()
         command = record_split[0]
         if command == "Enter" or command == "Change":
             user_nick[record_split[1]] = record_split[2]
             
     for record in records:
-        record_split = record.split(' ')
+        record_split = record.split()
         command = record_split[0]
         if command == "Enter":
-            comment = user_nick[record_split[1]] + "님이 들어왔습니다."
-            result.append(comment)
+            result.append(user_nick[record_split[1]] + "님이 들어왔습니다.")
         elif command == "Leave":
-            comment = user_nick[record_split[1]] + "님이 나갔습니다."
-            result.append(comment)
+            result.append(user_nick[record_split[1]] + "님이 나갔습니다.")
     return result
