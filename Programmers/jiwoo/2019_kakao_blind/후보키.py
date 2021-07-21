cfdc09c8c3975a284b81461dfda3b829e5ee366f
jjuyaa <smwujjuyaa@gmail.com>
2021-07-21 18:21:54 +0900

    [Programmers-42890] 후보키

--- Programmers/jiwoo/2019_kakao_blind/후보키.py
+++ Programmers/jiwoo/2019_kakao_blind/후보키.py
@@ -5,28 +5,29 @@ def solution(relations):
     col = len(relations[0])
     
     candidates = [[] for i in range(col+1)]
-    for i in range(col+1):
-        candidates[i] = combinations(range(col), i)
 
     min_key = []
     for i in range(1, col+1):
-        for j in candidates[i]:
+        candidates[i] = combinations(range(col), i)
+        for candidate in candidates[i]:
             not_key = 0
-            s = [[] for _ in range(row)]
-            for k in j:
-                for r in range(row):
-                    s[r].append(relations[r][k])
-            for i in s:
-                if s.count(i) >= 2:
+            unique = [[] for _ in range(row)]
+            # 유효성 검사
+            for k in candidate:
+                for r_ in range(row):
+                    unique[r_].append(relations[r_][k])
+            for i in unique:
+                if unique.count(i) >= 2:
                     not_key = 1
                     break
+            # 최소성 검사
             if not_key == 0:
-                if len(j) == 1:
-                    min_key.append(j)
+                if len(candidate) == 1:
+                    min_key.append(candidate)
                 else :
-                    for r in min_key:
-                        if set(r).issubset(set(j)):
+                    for r_ in min_key:
+                        if set(r_).issubset(set(candidate)):
                             break
                     else:
-                        min_key.append(j)
+                        min_key.append(candidate)
     return len(min_key)
