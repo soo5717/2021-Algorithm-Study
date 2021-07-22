8c58564b6e7c3743d9556d95622a8d3b0803fd27
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-07-23 00:31:09 +0900

    [Programmers-17680] 캐시

---
+++ Programmers/eugene/2018_kakao_blind/캐시.py
@@ -0,0 +1,20 @@
+def solution(cacheSize, cities):
+    answer = 0
+    my_cache=[]
+
+    #for i in range(len(cities)):cities[i]=cities[i].lower()
+        
+    if cacheSize ==0:
+        return len(cities)*5
+    
+    for city in cities:
+        city=city.lower()
+        if city in my_cache: #my_cache에 city가 있으면
+            answer+=1
+            my_cache.remove(city)
+        else: #없으면
+            answer+=5
+            if len(my_cache)==cacheSize:
+                del my_cache[0] #cache가 꽉 차있으면 맨 앞을 비우기
+        my_cache.append(city) #city추가.
+    return answer
