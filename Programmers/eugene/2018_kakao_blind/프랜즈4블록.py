13e51697105238a71ea98672102c4c50fc739532
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-07-30 16:25:16 +0900

    [Programmers-17679] 프렌즈4블록

--- Programmers/eugene/2018_kakao_blind/프랜즈4블록.py
+++ Programmers/eugene/2018_kakao_blind/프랜즈4블록.py
@@ -9,17 +9,19 @@ def solution(m, n, board):
                 if board[i][j] == board[i][j+1] == board[i+1][j] == board[i+1][j+1] != "0":
                     index_list+=[(i, j), (i, j+1),(i+1, j), (i+1, j+1)]
                     
-        if len(set(index_list)) ==0: #지울 블록 없으면 종료
+       
+        set_index_len= len(set(index_list))
+        if set_index_len ==0: #지울 블록 없으면 종료
             break
-        answer+=len(set(index_list))
+        answer+=set_index_len
     
         for x,y in index_list: #지울 블록 지우기
-            board[x][y]='0'
+            board[x][y]="0"
 
         for x,y in reversed(index_list):
             chk_drop=x-1
             while chk_drop >=0:
-                if board[x][y]=='0' and board[chk_drop][y] !="0":
+                if board[x][y]=="0" and board[chk_drop][y] !="0":
                     board[x][y]=board[chk_drop][y] #아래로 내리고
                     board[chk_drop][y]="0" #내린 부분 0으로 채우기
                     x-=1
