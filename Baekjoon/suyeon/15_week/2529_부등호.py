2d6ef2532ff7e70592d83dd0808a9cafaae23230
suyeon <soo5717@sookmyung.ac.kr>
2021-08-04 00:16:50 +0900

    [Baekjoon-2529] 부등호

--- Baekjoon/suyeon/15_week/2529_부등호.py
+++ Baekjoon/suyeon/15_week/2529_부등호.py
@@ -1,30 +1,34 @@
 # backtracking
-MIN_NUM = '9999999999'
-MAX_NUM = '0'
+MIN_NUM = 9999999999
+MAX_NUM = 0
 
-def backtracking(first, idx, answer, numbers, inequalities):
-  if(idx == len(inequalities)):
-    global MIN_NUM, MAX_NUM
-    answer_int = int(answer+str(first))
-    if answer_int < int(MIN_NUM): MIN_NUM = answer+str(first)
-    if answer_int > int(MAX_NUM): MAX_NUM = answer+str(first)
-    return
 
-  numbers[first] = True
-  for second in range(10):
-    if not numbers[second]:
-      if inequalities[idx] == '<' and first < second:
-        backtracking(second, idx + 1, answer + str(first), numbers, inequalities)
-      if inequalities[idx] == '>' and first > second:
-        backtracking(second, idx + 1, answer + str(first), numbers, inequalities)
-  numbers[first] = False
+def backtracking(first, idx, answer):
+    if idx == len(inequalities):
+        global MIN_NUM, MAX_NUM
+        answer = int(answer + str(first))
+        if answer < MIN_NUM:
+            MIN_NUM = answer
+        if answer > MAX_NUM:
+            MAX_NUM = answer
+        return
 
-k = input()
+    numbers[first] = True
+    for second in range(10):
+        if not numbers[second]:
+            if inequalities[idx] == '<' and first < second:
+                backtracking(second, idx + 1, answer + str(first))
+            if inequalities[idx] == '>' and first > second:
+                backtracking(second, idx + 1, answer + str(first))
+    numbers[first] = False
+
+
+k = int(input())
 inequalities = list(map(str, input().split()))
-numbers = [False] * 10 # 0 ~ 9
+numbers = [False] * 10  # 0 ~ 9
 
 for i in range(10):
-  backtracking(i, 0, '', numbers, inequalities)
+    backtracking(i, 0, '')
 
-print(MAX_NUM)
-print(MIN_NUM)
+print(str(MAX_NUM).zfill(k + 1))
+print(str(MIN_NUM).zfill(k + 1))
