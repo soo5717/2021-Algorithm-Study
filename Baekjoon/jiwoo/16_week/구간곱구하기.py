9fbe7311b847dfa3a7e1a0a766dc4f08a9732a1c
jjuyaa <smwujjuyaa@gmail.com>
2021-08-18 12:30:46 +0900

    [Baekjoon-11505] 구간 곱 구하기

--- Baekjoon/jiwoo/16_week/구간곱구하기.py
+++ Baekjoon/jiwoo/16_week/구간곱구하기.py
@@ -4,9 +4,16 @@ from math import *
 input = sys.stdin.readline
 NUM = 1000000007
 
+def init(node, start, end):
+    if start == end:
+        tree[node] = arr[start]
+        return tree[node] % NUM
+    mid= (start+end) // 2
+    tree[node] = init(node*2, start, mid) * init(node*2+1, mid+1, end) % NUM
+    return tree[node]
+
 def update(node, start, end, index, value):
-    if index < start or end < index:
-        return
+    if index < start or end < index: return
     if start == end:
         tree[node] = value
         return
@@ -15,7 +22,7 @@ def update(node, start, end, index, value):
     update(node*2+1, mid+1, end, index, value)
     tree[node] = (tree[node*2] * tree[node*2+1]) % NUM
     return tree[node]
-    
+	
 def query(node, start, end, left, right):
     if right < start or end < left:
         return 1
@@ -29,13 +36,15 @@ N, M, K = map(int, input().split())
 tree_size = 2**(ceil(log2(N)) + 1)
 
 tree = [1] * tree_size
+arr=[]
 for i in range(N):
-    num = int(input())
-    update(1, 0, N-1, i, num)
+	arr.append(int(input()))
+
+init(1, 0, N-1)
 
 for _ in range(M+K):
     a, b, c = map(int, input().split())
     if a == 1:
         update(1, 0, N-1, b-1, c)
-    elif a == 2:
+    else:
         print(query(1, 0, N-1, b-1, c-1))
