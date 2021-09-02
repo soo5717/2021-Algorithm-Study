455d9772a9524e4087472c1832d9509dff8cf06a
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-09-03 00:40:33 +0900

    [Baekjoon-2302] 극장 좌석

--- Baekjoon/eugene/18_week/극장 좌석.py
+++ Baekjoon/eugene/18_week/극장 좌석.py
@@ -6,15 +6,14 @@ if __name__ =="__main__":
 
     x, y = 1,2
     fibo = [1] * (n+1)
-    for i in range(1,n+1):
-        fibo[i] = x
-        x, y = y, x+y
-    
+    for i in range(2,n+1):
+        fibo[i] = fibo[i-1]+fibo[i-2]
     idx, answer = 1,1
     for _ in range(m):
         x = int(sys.stdin.readline())
         answer *= fibo[x-idx]
         idx = x+1
         
-    if idx < n : answer *= fibo[n-idx+1]
+    if idx < n :
+        answer *= fibo[n-idx+1]
     print(answer)
