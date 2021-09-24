c4b725b1407d9a5a3a1477643aca7a3989529706
suyeon <soo5717@sookmyung.ac.kr>
2021-09-24 22:52:08 +0900

    [Baekjoon-9322] 철벽 보안 알고리즘

---
+++ Baekjoon/suyeon/21_week/철벽 보안 알고리즘.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def decrypt():
+    public_key_dict = dict.fromkeys(public_key_2, 0)
+    for idx, key in enumerate(public_key_1):
+        public_key_dict[key] = idx
+
+    decrypted_cryptogram = [''] * N
+    for idx, value in enumerate(public_key_dict.values()):
+        decrypted_cryptogram[value] = cryptogram[idx]
+    return ' '.join(decrypted_cryptogram)
+
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        public_key_1 = input().split()
+        public_key_2 = input().split()
+        cryptogram = input().split()
+        print(decrypt())
