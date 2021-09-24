79584ebb1127b6d072f7156bbadff1355e642525
jjuyaa <smwujjuyaa@gmail.com>
2021-09-24 22:33:35 +0900

    [Baekjoon-9251] LCS

--- Baekjoon/jiwoo/20_week/LCS.py
+++ Baekjoon/jiwoo/20_week/LCS.py
@@ -1,13 +1,13 @@
 S1 = input()
 S2 = input()
 
-len_s1 = len(S1) + 1
-len_s2 = len(S2) + 1
+count_s1 = len(S1) + 1
+count_s2 = len(S2) + 1
 
-LCS = [[0] * (len_s1) for _ in range(len_s2)]
+LCS = [[0] * (count_s1) for _ in range(count_s2)]
 
-for i in range(1, len_s2):
-    for j in range(1, len_s1):
+for i in range(1, count_s2):
+    for j in range(1, count_s1):
         if S1[j-1] == S2[i-1]:
             LCS[i][j] = LCS[i-1][j-1] + 1
         else:
