4d0388b47f2b3bf03eb236ff990566121669c4c0
jjuyaa <smwujjuyaa@gmail.com>
2021-09-24 22:44:40 +0900

    [Baekjoon-11727] 2xn 타일링2

--- Baekjoon/jiwoo/20_week/2Xn타일링2.py
+++ Baekjoon/jiwoo/20_week/2Xn타일링2.py
@@ -3,6 +3,6 @@ N = int(input())
 DP = [0, 1, 3]
 
 for i in range(3, N+1):
-    DP.append((DP[i-2] * 2) + DP[i-1])
+    DP.append(((DP[i-2] * 2) + DP[i-1]) % 10007)
 
-print(DP[N] % 10007)
+print(DP[N])
