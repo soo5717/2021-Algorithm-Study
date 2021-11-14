5803134846429955a2afde2e53b5b9d748c31c67
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-11-15 00:27:22 +0900

    [Baekjoon-2606] 바이러스

--- Baekjoon/eugene/23_week/바이러스.py
+++ Baekjoon/eugene/23_week/바이러스.py
@@ -1,27 +1,29 @@
 import sys
+from collections import deque
+
+
+def bfs(graph, start):
+    queue = deque([start])
+    visited[start] = True
+    cnt_virus = 0
+    while queue:
+        v = queue.popleft()
+        for idx in graph[v]:
+            if not visited[idx]:
+                queue.append(idx)
+                visited[idx] = True
+                cnt_virus += 1
+    return cnt_virus
+
 
 node_n, edge_n = int(input()), int(input())
-graph = {}
 
-
-def dfs(graph, startV):
-    s = [startV]
-    visited = []
-    while s:
-        nowV = s.pop()
-        if nowV not in visited:
-            visited.append(nowV)
-            s.extend(graph[nowV][::-1])
-    return len(visited)-1
-
-
-for i in range(1, node_n+1):
-    graph[str(i)] = []
+visited = [False] * (node_n+1)
+graph = [[] for _ in range(node_n + 1)]
 
 for _ in range(edge_n):
-    start, end = sys.stdin.readline().split()
-    v1, v2 = graph[start], graph[end]
-    v1.append(end)
-    v2.append(start)
+    start, end = map(int, sys.stdin.readline().split())
+    graph[start].append(end)
+    graph[end].append(start)
 
-print(dfs(graph, '1'))
+print(bfs(graph, 1))
