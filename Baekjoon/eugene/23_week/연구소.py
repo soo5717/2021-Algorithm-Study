2ce50a9b153493b4b046b808c880c3c2224c3227
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-11-15 00:31:39 +0900

    [Baekjoon-14502] 연구소

--- Baekjoon/eugene/23_week/연구소.py
+++ Baekjoon/eugene/23_week/연구소.py
@@ -2,6 +2,8 @@ import copy
 from itertools import combinations
 from collections import deque
 
+EMPTY, WALL, VIRUS = '0', '1', '2'
+
 moving = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 empty_list, virus_list = [], []
 lab_map = []
@@ -18,9 +20,9 @@ def bfs(new_map):
             my = vy + moving[i][0]
             if my < 0 or my >= row or mx < 0 or mx >= col:
                 continue
-            if new_map[my][mx] == '0' and not visited[my][mx]:
+            if new_map[my][mx] == EMPTY and not visited[my][mx]:
                 queue.append([my, mx])
-                new_map[my][mx] = '2'
+                new_map[my][mx] = VIRUS
                 visited[my][mx] = True
                 infect_cnt += 1
 
@@ -33,24 +35,23 @@ for i in range(row):
     lab_map.append(temp)
 
     for j in range(col):
-        if temp[j] == '0':
+        if temp[j] == EMPTY:
             empty_list.append([i, j])
-        elif temp[j] == '2':
+        elif temp[j] == VIRUS:
             virus_list.append([i, j])
 
 max_safe_area = 0
-for new_wall in list(combinations(empty_list, 3)):
-    empty1, empty2, empty3 = new_wall
+for empty1, empty2, empty3 in list(combinations(empty_list, 3)):
     y1, x1 = empty1
     y2, x2 = empty2
     y3, x3 = empty3
 
     new_map = copy.deepcopy(lab_map)
-    new_map[y1][x1] = '1'
-    new_map[y2][x2] = '1'
-    new_map[y3][x3] = '1'
+    new_map[y1][x1] = WALL
+    new_map[y2][x2] = WALL
+    new_map[y3][x3] = WALL
 
     safe_area = bfs(new_map)
-    max_safe_area = safe_area if max_safe_area < safe_area else max_safe_area
+    max_safe_area = max(safe_area, max_safe_area)
 
 print(max_safe_area)
