7264b3c5c795b10ac121238aedde929a7116ab65
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-12-05 17:20:05 +0900

    [Programmers-67257] 수식 최대화

---
+++ Programmers/eugene/26_week/수식 최대화.py
@@ -0,0 +1,40 @@
+from collections import deque
+import copy
+
+def solution(expression):
+    answer = 0
+    operator_case = [["+", "-", "*"], ["+", "*", "-"], 
+            ["-", "+", "*"],["-", "*", "+"],
+            ["*", "+", "-"],["*", "-", "+"]]
+    
+    temp=""
+    operand, operator= [], []
+    for e in expression:
+        if not e.isdigit():
+            operand.append(temp)
+            operator.append(e)
+            temp = ""
+        else:
+            temp += e
+    operand.append(temp)
+    
+    max_num = 0
+    for case in operator_case:
+        temp_operand = copy.deepcopy(operand)
+        temp_operator = copy.deepcopy(operator)
+
+        for c in case:
+            while c in temp_operator:
+                idx = temp_operator.index(c)
+                temp_operand[idx] = str(eval(temp_operand[idx]+ c+temp_operand[idx+1]))
+                
+                del temp_operand[idx+1]
+                del temp_operator[idx]
+                
+        max_num = max(max_num, abs(int(temp_operand[0])))
+    return max_num
+
+def cal(a, b, c):
+    if b == '+': return a+c
+    elif b =='*': return a*c
+    else: return a-c
