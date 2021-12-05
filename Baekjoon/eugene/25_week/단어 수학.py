a205e38f4d375d434d3192bc34d837b693632e19
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2021-12-05 17:08:01 +0900

    [Baekjoon-1339] 단어 수학

--- Baekjoon/eugene/25_week/단어 수학.py
+++ Baekjoon/eugene/25_week/단어 수학.py
@@ -7,10 +7,10 @@ for _ in range(n):
     word = list(input())
     for idx, value in enumerate(word):
         alphabet_num[value] = 0
-        num_list[len(word) - idx] += {value}
+        num_list[len(word) - idx] += value
 
 for key in range(9, -1, -1):
-    for num in (num_list[key]):
+    for num in num_list[key]:
         alphabet_num[num] += 10**(key-1)
 
 sorted_alphabet = sorted(alphabet_num.items(), key = lambda item: item[1], reverse=True)
