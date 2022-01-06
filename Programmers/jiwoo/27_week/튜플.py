af68d022eb71ba620d14f93ebe950f0e165c11c3
jjuyaa <smwujjuyaa@gmail.com>
2022-01-06 23:08:23 +0900

    [Programmers-64065] 튜플

---
+++ Programmers/jiwoo/27_week/튜플.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = []
+    s = s[2:-2]
+    s = s.split("},{")
+    s.sort(key = len)
+    for i in s:
+        i = i.split(",")
+        for j in i:
+            if int(j) not in answer:
+                answer.append(int(j))
+                print(answer)
+    return answer
