707bbfe0c211bcdda784b1729cdec3f7bb5092fc
eugene_tp <54016224+eugene-tp@users.noreply.github.com>
2022-06-22 19:34:19 +0900

    [Programmers-62048] 멀쩡한 사각형

--- Programmers/eugene/32_week/멀쩡한 사각형.py
+++ Programmers/eugene/32_week/멀쩡한 사각형.py
@@ -1,8 +1,7 @@
-def solution(w,h):
-    return w * h - (w + h - gcd(w, h))
-
-
 def gcd(x, y):
     while y:
         x, y = y, x % y
     return x
+
+def solution(w,h):
+    return w * h - (w + h - gcd(w, h))
